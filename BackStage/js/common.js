e31ee2070aae2ae8847c10e44767b2f59c51c6f6
hong007 <phi007@126.com>
2016-05-05 00:03:46 +0800

    按设计图调整

--- BackStage/js/common.js
+++ BackStage/js/common.js
@@ -1,3 +1,16 @@
+// iframe高度自适应
+function iFrameHeight() {
+	var iframeid = document.getElementById("iframe"); //iframe id
+	if (document.getElementById) {
+		if (iframeid && !window.opera) {
+			if (iframeid.contentDocument && iframeid.contentDocument.body.offsetHeight) {
+				iframeid.height = iframeid.contentDocument.body.offsetHeight;
+			} else if (iframeid.Document && iframeid.Document.body.scrollHeight) {
+				iframeid.height = iframeid.Document.body.scrollHeight;
+			}
+		}
+	}
+}
 // 岗位选择
 function addMark(obj) {
 	// alert(obj.is(".hiretype"));
@@ -5,78 +18,85 @@ function addMark(obj) {
 	if (obj.is(".hiretype") !== true) {
 		// alert(12);
 		obj.siblings("a").removeClass("choosemark");
-	}else{
-		var txt=obj.text();
-		var curIn=obj.attr("data-index");
-		var html="";
-		if (obj.is(".choosemark")==true) {
-				// alert(1);
-				html='<a href=\"javascript:void(0);\" data-rtIndex='+curIn+' onclick=\"delResult($(this))\">'+txt+ '<span>×</span> </a>';
-			$("#chooseResult").append(html);	
-		}else{
+	} else {
+		var txt = obj.text();
+		var curIn = obj.attr("data-index");
+		var html = "";
+		if (obj.is(".choosemark") == true) {
+			// alert(1);
+			html = '<a href=\"javascript:void(0);\" data-rtIndex=' + curIn + ' onclick=\"delResult($(this))\">' + txt + '<span>×</span> </a>';
+			$("#chooseResult").append(html);
+		} else {
 			// alert(2);
-			$("[data-rtIndex="+curIn+"]").remove();
+			$("[data-rtIndex=" + curIn + "]").remove();
 		}
 	}
 }
 
 // 清除岗位选中项
-function delResult(a){
-	var curRtIndex=a.attr("data-rtIndex");
+function delResult(a) {
+	var curRtIndex = a.attr("data-rtIndex");
 	a.remove();
-	$("[data-index="+curRtIndex+"]").removeClass("choosemark");
+	$("[data-index=" + curRtIndex + "]").removeClass("choosemark");
 }
 
 //默认弹出框
 var oDialog = null;
-function dialogModality(url, toptitlecn, areaSize,btnName) {
-    if (!areaSize)
-        // areaSize = ['490px', '273px'];
-        areaSize = ['518px', '218px'];
 
-    if(!btnName)
-    	btnName = ['确定'];
-    var  cancelB=false;
+function dialogModality(url, toptitlecn, areaSize, btnName) {
+		var toptitlecn="";
+		var cancelValP="取消"
+	if (!areaSize)
+	// areaSize = ['490px', '273px'];
+		areaSize = ['518px', '218px'];
 
-    if (btnName[1]=="取消") {
-    	cancelB=true;
-    }
+	if (!btnName)
+		btnName = ['确定'];
+	var cancelB = false;
 
-    oDialog = $.dialog({
-        skin: 'gray',
-        width: areaSize[0],
-        height: areaSize[1],
-        title: toptitlecn,
-        lock: true,
-        opacity: 0.5,
-        max:false,
-        min:false,
-        cancelVal: '取消',
-        cancel:  cancelB,
-        // 自定义按钮
-        button: [
-	        {
-	            // name: '继续看',
-	            name: btnName[0],
-	        }
-	        // ,
-	        // {
-	        //     name: btnName[1]
-	        // }
-	    ],
-        content: 'url:' + url
-        
-    });
+	if (btnName[1] == "取消") {
+		cancelB = true;
+	}
+	if (btnName[0] == "关闭") {
+		cancelB = true;
+		cancelValP="下一个";
+	}
+
+	oDialog = $.dialog({
+		skin: 'gray',
+		width: areaSize[0],
+		height: areaSize[1],
+		// title: false,
+		title: toptitlecn,
+		lock: true,
+		opacity: 0.5,
+		max: false,
+		min: false,
+		cancelVal: cancelValP,
+		cancel: cancelB,
+		// 自定义按钮
+		button: [{
+				// name: '继续看',
+				name: btnName[0],
+			}
+			// ,
+			// {
+			//     name: btnName[1]
+			// }
+		],
+		content: 'url:' + url
+
+	});
 }
 
 $(function() {
 	// 选择岗位范围
-	$("#chooseSpan").click(function(){
-        $(".hiretype-content").toggle();
-    });
-    $(".confirm-type").click(function(){
-        $(".hiretype-content").hide();
-    });
+	$("#chooseSpan").click(function() {
+		$(".hiretype-content").toggle();
+	});
+	$(".confirm-type").click(function() {
+		$(".hiretype-content").hide();
+	});
 
 
 	// 时间日期选择
